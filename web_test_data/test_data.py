39776c584e3832f8b141bf4a81f41965ffde1f98
shayna <shaynachen0917@gmail.com>
2023-04-05 21:04:54 +0800

    测试数据

---
+++ web_test_data/test_data.py
@@ -0,0 +1,7 @@
+# 测试数据
+url = {"url": "http://erp.lemfix.com/login.html"}
+login_data = [
+    {"username": "test123"},
+    {"password": "123456"}
+]
+search_word = {"search_word": "LSCK00000009258"}
